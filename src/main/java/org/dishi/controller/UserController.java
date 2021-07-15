034dc54bb0bd15b9417762d7063dccb8b2e70dfc
tianshiyu <1055052061@qq.com>
2021-07-15 11:27:14 +0800

    集成swagger2，生成api接口文档

--- src/main/java/org/dishi/controller/UserController.java
+++ src/main/java/org/dishi/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.dishi.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.dishi.common.response.ServerResponse;
 import org.dishi.domain.User;
@@ -10,16 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("user")
+@Api("用户相关接口")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("login")
+    @ApiOperation("登录接口")
     public ServerResponse<String> login(@RequestBody User user){
         return userService.login(user);
     }
 
     @GetMapping("hello")
+    @ApiOperation("登录测试接口")
     public ServerResponse<String> hello(){
         return ServerResponse.createBySuccessMessage("hello");
     }
